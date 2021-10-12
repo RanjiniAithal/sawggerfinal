1b7622523f038875a7e5c39cf32a488b9f01d96a
Ranjini <ranjiniaithal1@gmail.com>
2021-10-12 21:57:52 +0530

    ff

--- src/index.ts
+++ src/index.ts
@@ -20,6 +20,7 @@ app.use(
 app.use("/api", require("./routes/v1/app"));
 app.get("/", (req:any, res:any) => {
   res.send("Helloooooooooooooo");
+  console.log("api hit.. ")
 });
 
 app.listen(8082, () => console.log(`Server is running on port 8082`));
